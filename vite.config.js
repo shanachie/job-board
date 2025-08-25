c319465e675b1e0b9f86895ef7ba0a2b5697ee51
shanachie <marco.masini85@gmail.com>
2025-08-25 20:23:05 +0200

    Force Vite to bind on ipv4 only

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: 'localhost',
+        port: 5173,
+        strictPort: true,
+        hmr : {
+            host : 'localhost'
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
