2dcebf0082844392b87dbb593adcd7bf62522b3f
shanachie <marco.masini85@gmail.com>
2025-08-30 11:50:42 +0200

    Estabilishes model relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
     /** @use HasFactory<\Database\Factories\JobFactory> */
     use HasFactory;
+
+    public function employer(): belongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
