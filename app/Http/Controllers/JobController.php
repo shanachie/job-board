587b914d1b02476500365b40666e87dede74374d
shanachie <marco.masini85@gmail.com>
2025-08-30 22:59:52 +0200

    Fixes Jobs showing order

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -17,10 +17,10 @@ class JobController extends Controller
      */
     public function index()
     {
-        $jobs = Job::all()->groupBy('featured');
+        $jobs = Job::latest()->get()->groupBy('featured');
         return view('jobs.index', [
-            'featuredJobs' => $jobs[0],
-            'jobs' => $jobs[1],
+            'jobs' => $jobs[0],
+            'featuredJobs' => $jobs[1],
             'tags' => Tag::all()
         ]);
     }
