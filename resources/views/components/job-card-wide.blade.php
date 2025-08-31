6eee963068bb8d5b93800045a699104a9417de49
shanachie <marco.masini85@gmail.com>
2025-08-31 22:13:57 +0200

    Refactors job cards

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -7,14 +7,16 @@
 <x-panel class="flex gap-x-6">
 
     <div>
-        <x-employer-logo :employer="$job->employer" />
+        <a href="/employers/{{ $job->employer->name }}">
+            <x-employer-logo :employer="$job->employer"/>
+        </a>
     </div>
 
 
     <div class="flex-1 flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-600">{{ $job->employer->name }}</a>
+        <a href="/employers/{{ $job->employer->name }}" class="self-start text-sm text-gray-600">{{ $job->employer->name }}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">
+        <h3 class="font-bold text-xl mt-3 hover:text-blue-800 transition-colors duration-300">
             <a href="{{ $job->url }}" target="_blank">
                 {{ $job->title }}
             </a>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -4,16 +4,27 @@
     /** @var App\Models\Job $job */
 @endphp
 
-<x-panel class="flex flex-col text-center">
+<x-panel class="flex flex-col">
 
-    <div class="self-start text-sm">{{ $job->employer->name }}</div>
-    <div class="py-8">
-        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">
+    <div class="self-start w-full group/employer">
+        <a class="flex space-x-3" href="/employers/{{ $job->employer->name }}">
+            <x-employer-logo :employer="$job->employer" :width="36" />
+            <div class="text-sm w-fit self-center group-hover/employer:text-gray-600 transition-colors duration-300">
+                    {{ $job->employer->name }}
+            </div>
+        </a>
+    </div>
+
+    <div class="py-8 text-center">
+
+        <h3 class="hover:text-blue-800 text-xl font-bold w-full transition-colors duration-300">
             <a href="{{ $job->url }}" target="_blank">
                 {{ $job->title }}
             </a>
         </h3>
-        <p class="text-sm mt-4">{{ $job->salary }}</p>
+
+        <p class="text-sm mt-4">{{ $job->location }}</p>
+        <p class="text-sm">{{ $job->salary }}</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
@@ -22,9 +33,6 @@
                 <x-tag size="small" :tag="$tag" />
             @endforeach
         </div>
-
-        <x-employer-logo :employer="$job->employer" :width="42" />
-
     </div>
 
 </x-panel>
